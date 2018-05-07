b97f415023fd60a9d9b56ddb0367c18132d113f6
kilic <kiliconu@itu.edu.tr>
2018-05-07 18:09:37 +0300

    signer pre-claim signature scheme is added

--- contracts/Forwarder.sol
+++ contracts/Forwarder.sol
@@ -39,7 +39,18 @@ contract Forwarder{
     emit Forwarded(sender, dst, data, success);
   }
 
-  function forward_3(address dst, bytes data, uint nonce, bytes sig) public { 
+  function forward_3(address signer, address dst, bytes data, uint timeout, uint nonce, bytes sig) public { 
+    bytes32 dataHash = keccak256(signer, dst, data, msg.sender, timeout, nonce);
+    address _signer = dataHash.recover2(sig);
+    require(_signer == signer);
+    require(nonce > relayerNonce[signer][msg.sender]);
+    require(timeout > block.timestamp);
+    relayerNonce[signer][msg.sender] = nonce;
+    bool success = fwd(dst, data);
+    emit Forwarded(signer, dst, data, success);
+  }
+
+  function forward_4(address dst, bytes data, uint nonce, bytes sig) public { 
     bytes32 dataHash = keccak256(dst,data,nonce);
     address sender = dataHash.recover2(sig);
     require(nonce > senderNonce[sender]);
@@ -56,7 +67,7 @@ contract Forwarder{
     emit Forwarded(sender, dst, calldata, success);
   }
 
-  function forward_4(address dst, bytes data, uint timeout, uint nonce, bytes sig) public { 
+  function forward_5(address dst, bytes data, uint timeout, uint nonce, bytes sig) public { 
     bytes32 dataHash = keccak256(dst, data, msg.sender, timeout, nonce);
     address sender = dataHash.recover2(sig);
     require(nonce > relayerNonce[sender][msg.sender]);
@@ -72,6 +83,25 @@ contract Forwarder{
     BytesOp.copy(ptr1 + SELECTOR_SIZE, dest + SELECTOR_SIZE + encodedAddress.length, data.length - SELECTOR_SIZE);
     bool success = fwd(dst, calldata);
     emit Forwarded(sender, dst, calldata, success);
+  }
+
+  function forward_6(address signer, address dst, bytes data, uint timeout, uint nonce, bytes sig) public { 
+    bytes32 dataHash = keccak256(signer, dst, data, msg.sender, timeout, nonce);
+    address _signer = dataHash.recover2(sig);
+    require(signer == _signer);
+    require(nonce > relayerNonce[signer][msg.sender]);
+    require(timeout > block.timestamp);
+    relayerNonce[signer][msg.sender] = nonce;
+    uint ptr1 = BytesOp.dataPtr(data);
+    bytes memory encodedAddress = abi.encode(signer);
+    uint ptr2 = BytesOp.dataPtr(encodedAddress);
+    bytes memory calldata = new bytes(data.length + encodedAddress.length);
+    uint dest = BytesOp.dataPtr(calldata);
+    BytesOp.copy(ptr1, dest, SELECTOR_SIZE);
+    BytesOp.copy(ptr2, dest + SELECTOR_SIZE, encodedAddress.length);
+    BytesOp.copy(ptr1 + SELECTOR_SIZE, dest + SELECTOR_SIZE + encodedAddress.length, data.length - SELECTOR_SIZE);
+    bool success = fwd(dst, calldata);
+    emit Forwarded(signer, dst, calldata, success);
   }
 
   function fwd(address dst, bytes calldata) internal returns(bool success){
